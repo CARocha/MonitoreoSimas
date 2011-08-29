e92f9289900286330489886e5716bc552a83a1c1
sacrac <you@example.com>
2011-08-29 16:18:10 -0600

    fix again

--- monitoreo/simas/urls.py
+++ monitoreo/simas/urls.py
@@ -6,6 +6,10 @@ from models import Encuesta
 
 urlpatterns = patterns('monitoreo.simas.views',
     (r'^index/$', 'inicio'),
+    (r'^ajax/organi/$', 'get_organi'),
+    (r'^ajax/munis/$', 'get_munis'),
+    (r'^ajax/comunies/$', 'get_comunies'),
+    #viejas urls
     (r'^index/ajax/organizaciones/(?P<departamento>\d+)/$', 'get_organizacion'),
     (r'^index/ajax/municipio/(?P<departamento>\d+)/$', 'get_municipios'),
     (r'^index/ajax/comunidad/(?P<municipio>\d+)/$', 'get_comunidad'),
